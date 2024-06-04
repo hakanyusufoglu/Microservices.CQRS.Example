fc6513a754e94b6fa2aaddf28f76f5ce71a254d4
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-04 16:17:14 +0300

    CQRS patterni uygulayabilmek için uygun endpointler gerçekleştirildi.

---
+++ Microservices.Tutorial.CQRS.Example/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microservices.Tutorial.CQRS.Example.ManuelCQRS.Commands.Requests;
+using Microservices.Tutorial.CQRS.Example.ManuelCQRS.Handlers.CommandHandlers;
+using Microservices.Tutorial.CQRS.Example.ManuelCQRS.Handlers.QueryHandlers;
+using Microservices.Tutorial.CQRS.Example.ManuelCQRS.Queries.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Tutorial.CQRS.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(CreateProductCommandHandler createProductCommandHandler, DeleteProductCommandHandler deleteProductCommandHandler, GetAllProductQueryHandler getAllProductQueryHandler, GetByIdProductQueryHandler getByIdProductQueryHandler)
+     : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] GetAllProductQueryRequest request)
+            => Ok(getAllProductQueryHandler.GetAllProduct(request));
+
+        [HttpGet("{ProductId}")]
+        public IActionResult Get([FromRoute] GetByIdProductQueryRequest request)
+         => Ok(getByIdProductQueryHandler.GetByIdProduct(request));
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProductCommandRequest request)
+      => Ok(createProductCommandHandler.CreateProduct(request));
+
+        [HttpDelete("{ProductId}")]
+        public IActionResult Delete([FromRoute] DeleteProductCommandRequest request)
+          => Ok(deleteProductCommandHandler.DeleteProduct(request));
+
+    }
+}
--- Microservices.Tutorial.CQRS.Example/Program.cs
+++ Microservices.Tutorial.CQRS.Example/Program.cs
@@ -1,15 +1,21 @@
 using Microservices.Tutorial.CQRS.Example.ManuelCQRS.Handlers.CommandHandlers;
 using Microservices.Tutorial.CQRS.Example.ManuelCQRS.Handlers.QueryHandlers;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+#region Manuel CQRS
 builder.Services.AddSingleton<CreateProductCommandHandler>()
-                .AddSingleton<DeleteProductCommandHandler>()
-                .AddSingleton<GetAllProductQueryHandler>()
-                .AddSingleton<GetByIdProductQueryHandler>();
+        .AddSingleton<DeleteProductCommandHandler>()
+        .AddSingleton<GetAllProductQueryHandler>()
+        .AddSingleton<GetByIdProductQueryHandler>();
+#endregion
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -20,6 +26,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
